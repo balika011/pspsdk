ff4c5ed2b6509770d92e580a9193f452475d4ad5
wally4000 <wally4000@users.noreply.github.com>
2021-12-05 07:50:58 +0000

    deploy: 1fefe3163576f592d7caf4028bc5ff3340f7c689

---
+++ navtreedata.js
@@ -0,0 +1,82 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PSPSDK", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"gdb-kernellib_8c.html#aeeab7ed2321b1e1ec556e9871f8cfeaf",
+"group__Ctrl.html#ggac080131ea3904c97efb6c31b1c4deb10a17d94c81e78bb8152fcbec71514871c9",
+"group__GU.html#ga01a1668e175f969f9b958545f62b7c20",
+"group__GU.html#gaec7e1497765bb4d74cd9ef8ad0f3d277",
+"group__ModuleMgr.html#ga3e50ae40e67fb6172ab7d86e87d3c464",
+"group__Syscon.html#ga70e2ae1f187b8b79ffffc3265171304c",
+"group__ThreadMan.html#gab81151d04f6509423c8fa3545f50fa2f",
+"group__UMD.html#gaf9de0761e684f45874f77cab817e1003",
+"inet_8h.html#ad15d5a2e4b1a1336ad989ebe7fa1ddcf",
+"pspdebugkb_8h.html#aed2cefe11d683ced79b15ebf93707600",
+"pspgu_8h.html#a06411ac498e27564c60d54734c1d6be4",
+"psphprm_8h.html",
+"pspkerror_8h.html#ada38ce7dd0ddff755524e736afd6fcaca2f88c74c5a37cf2b316c4552b34053b3",
+"pspmpeg_8h.html#a4ce1a529ddef8898a0522f0c03d0099a",
+"psppower_8h.html#a4a87d2d5bf941ea737df7faae8736248",
+"pspusb_8h.html",
+"psputility__htmlviewer_8h.html#a7689d052af51051824a105a2c9ca76a2a9abc376a96473c6e16366cea19aadf86",
+"psputils_8h.html",
+"socket_8h.html#a7a40a39aa9dfaf771780ebe061ffa940",
+"structPspIoDrvFuncs.html#af5df9e0a4b49a4bcaeaff58cfc67b0b2",
+"structSceKernelThreadInfo.html#a29c32a2783782173d9ccd785aece33ec",
+"structScePspFMatrix4Unaligned.html#a7a3ec8bdbdc992705b612cc1222ce65a",
+"struct__PspDebugRegBlock.html#a7726cabdb09f162a7c9d0b652c556a9b",
+"structpspUtilityHtmlViewerParam.html#ae97f24ad8ee968edb464b4c2117f6998"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
